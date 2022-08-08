ed06ff598575d8056db676c6e3a1d6b7de28639f
박정규 <srk99279@naver.com>
2022-08-08 19:56:08 +0900

    selectionSort

---
+++ sort/selectionSort/selectionSort.js
@@ -0,0 +1,20 @@
+function selectionSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var lowest = i;
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowest]) {
+                lowest = j;
+            }
+        }
+        if (i !== lowest) {
+            var tmp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = tmp;
+        }
+
+    }
+    console.log(arr)
+    return arr;
+}
+
+selectionSort([5, 3, 1, 2, 4])
