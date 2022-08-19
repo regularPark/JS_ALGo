a096695ffc07ef3acacd702ce176405f8f6b8db4
박정규 <srk99279@naver.com>
2022-08-19 17:21:36 +0900

    Create binarySearchTree.js

---
+++ dataStructure/tree/binarySearchTree.js
@@ -0,0 +1,67 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(val) {
+        let newNode = new Node(val);
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            let current = this.root;
+            while (true) {
+                if (val === current.val) return undefined;
+                if (val < current.val) {
+                    if (current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (val > current.val) {
+                    if (current.right === null) {
+                        current.right = newNode
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+
+        }
+    }
+    contains(val) {
+        if (!this.root) return false;
+        let current = this.root;
+        let found = false;
+        while (current && !found) {
+            if (val < current.val) {
+                current = current.left;
+            } else if (val > current.val) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(15);
+tree.insert(5);
+tree.insert(9);
+tree.insert(12);
+tree.contains(1);
+tree.contains(12);
+
+// Insertion - O(log n)
+// Searching - O(log n)
