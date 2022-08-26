df1cd271a56326fa6f50fe2d78599ccc21e608fd
박정규 <srk99279@naver.com>
2022-08-26 11:07:20 +0900

    Update graph.js

--- dataStructure/graph/graph.js
+++ dataStructure/graph/graph.js
@@ -24,22 +24,88 @@ class Graph {
         }
         delete this.adjacencyList[vertex]
     }
+    depthFirstRecursive(start) {
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex) {
+            if (!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach((neighbor) => {
+                if (!visited[neighbor]) {
+                    return dfs(neighbor);
+                }
+            })
+        })(start);
+        console.log(result)
+        return result;
+    }
+    depthFirstIterative(start) {
+        const stack = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[start] = true;
+        while (stack.length) {
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor)
+                }
+            })
+        }
+        console.log(result);
+        return result;
+    }
+    breadthFirst(start) {
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+        visited[start] = true;
+
+        while (queue.length) {
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            })
+        }
+        console.log(result);
+        return result;
+    }
 }
 
 let g = new Graph();
-g.addVertex('Seoul')
-g.addVertex('Dallas')
-g.addVertex('Tokyo')
-g.addVertex('Busan')
-g.addVertex('LA')
-g.addVertex('NY')
-g.addEdge('Seoul', 'Tokyo')
-g.addEdge('Seoul', 'Dallas')
-g.addEdge('Busan', 'Seoul')
-g.addEdge('LA', 'Seoul')
-g.addEdge('LA', 'NY');
-g.removeVertex('Seoul')
+g.addVertex('A')
+g.addVertex('B')
+g.addVertex('C')
+g.addVertex('D')
+g.addVertex('E')
+g.addVertex('F')
 
+g.addEdge('A', 'B')
+g.addEdge('A', 'C')
+g.addEdge('B', 'D')
+g.addEdge('C', 'E')
+g.addEdge('D', 'E')
+g.addEdge('D', 'F')
+g.addEdge('E', 'F')
+
+// g.depthFirstRecursive('A')
+// g.depthFirstIterative('A');
+
+g.breadthFirst('A')
 // g.removeEdge('Seoul', 'Dallas')
 
 
